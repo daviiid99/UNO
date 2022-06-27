5e70c69b678a0e11b9ae5955bb65bbc16eab7552
daviiid99 <daviiideveloper@gmail.com>
2022-06-27 15:17:25 +0200

    Update source

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ from player import *
 
 # --------- Global variables ------------
 
-version = "1.5"
+version = "1.6"
 
 # ------------------------------
 
@@ -71,7 +71,7 @@ def create_title_screen_animation (LEFT_CARD, CENTER_CARD, RIGHT_CARD) :
     WIN.blit(RIGHT_CARD, (680, 340))
     WIN.blit(CENTER_CARD, (450, 280))
 
-    version = small_font.render("v1.5", 1, WHITE)
+    version = small_font.render(version, 1, WHITE)
     WIN.blit(version, (20, 20) )
 
     if updated :
